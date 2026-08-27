185d0ea3c7a9d5f922b8ce12134e9e05759b9026
Derell Licht <derelict@comcast.net>
2026-08-27 09:56:37 -0700

    fixing libhhctrl32.a

--- html_libgen.cmd
+++ html_libgen.cmd
@@ -1,8 +1,7 @@
 gendef C:\Windows\System32\hhctrl.ocx
-x86_64-w64-mingw32-dlltool --input-def hhctrl.def --output-lib libhhctrl64.a --dllname hhctrl.ocx
-llvm-objdump -f libhhctrl64.a
+x86_64-w64-mingw32-dlltool --kill-at --input-def hhctrl.def --output-lib libhhctrl64.a --dllname hhctrl.ocx
+move hhctrl.def hhctrl64.def
 
 gendef C:\Windows\SysWOW64\hhctrl.ocx
-i686-w64-mingw32-dlltool --input-def hhctrl.def --output-lib libhhctrl32.a --dllname hhctrl.ocx
-llvm-objdump -f libhhctrl32.a
-
+i686-w64-mingw32-dlltool --kill-at --input-def hhctrl.def --output-lib libhhctrl32.a --dllname hhctrl.ocx
+move hhctrl.def hhctrl32.def
